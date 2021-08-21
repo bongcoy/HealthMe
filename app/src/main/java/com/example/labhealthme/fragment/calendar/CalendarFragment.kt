ef495beb1aca73924b156cb74bcf53a9bf7dae03
Cal <mpascaldewantara@gmail.com>
2021-08-21 13:07:33 +0700

    add Calendar Fragment

---
+++ app/src/main/java/com/example/labhealthme/fragment/calendar/CalendarFragment.kt
@@ -0,0 +1,19 @@
+package com.example.labhealthme.fragment.calendar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.labhealthme.R
+
+class CalendarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+}
