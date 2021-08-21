f91d12fd6a6935bd27292d56673e7d74e5dceb6d
Cal <mpascaldewantara@gmail.com>
2021-08-21 11:58:40 +0700

    add DiagnoseActivity

---
+++ app/src/main/java/com/example/labhealthme/diagnose/DiagnoseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.labhealthme.diagnose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.labhealthme.R
+
+class DiagnoseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diagnose)
+    }
+}
